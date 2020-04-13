56ebcd32dc95adbb2cac438a5a8c849e6c3fec44
GvidsTV <62351613+GvidsTV@users.noreply.github.com>
2020-04-13 06:42:10 -0400

    Add files via upload

---
+++ Exercise4/Student.java
@@ -0,0 +1,12 @@
+
+public class Student extends Person {
+    public double gpa;
+
+    public Student(String name,int age,double gpa){
+        super(name, age);
+        this.gpa=gpa;
+    }
+    
+
+
+}
---
+++ Exercise4/StudyGroup.java
@@ -0,0 +1,18 @@
+
+public class StudyGroup {
+  
+    public static void main(String[] args) {
+        //stage
+
+        Person p1= new Person("Gareth",34);
+        Student s1= new Student("Mark",33,4.0);
+
+        System.out.println(p1.name + " " + p1.age);
+        System.out.println(s1.name+" "+s1.gpa);
+    
+
+}
+
+
+
+}
